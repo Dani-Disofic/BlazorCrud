3b953a627912062ac04bccdde51ae636ef197201
Dani-Disofic <disoficdani@gmail.com>
2023-08-05 13:38:46 +0200

    Subiendo todos los archivos del proyecto al repositorio

---
+++ BlazorCrud.Server/Models/DbcrudBlazorContext.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorCrud.Server.Models;
+
+public partial class DbcrudBlazorContext : DbContext
+{
+    public DbcrudBlazorContext()
+    {
+    }
+
+    public DbcrudBlazorContext(DbContextOptions<DbcrudBlazorContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Departamento> Departamentos { get; set; }
+
+    public virtual DbSet<Empleado> Empleados { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Departamento>(entity =>
+        {
+            entity.HasKey(e => e.IdDepartamento).HasName("PK__departam__64F37A1695F01CE6");
+
+            entity.ToTable("departamento");
+
+            entity.Property(e => e.IdDepartamento).HasColumnName("id_departamento");
+            entity.Property(e => e.Nombre)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+        });
+
+        modelBuilder.Entity<Empleado>(entity =>
+        {
+            entity.HasKey(e => e.IdEmpleado).HasName("PK__Empleado__88B51394C0A9A901");
+
+            entity.ToTable("Empleado");
+
+            entity.Property(e => e.IdEmpleado).HasColumnName("id_empleado");
+            entity.Property(e => e.FechaAlta).HasColumnType("date");
+            entity.Property(e => e.IdDepartamento).HasColumnName("id_departamento");
+            entity.Property(e => e.NombreCompleto)
+                .HasMaxLength(50)
+                .IsUnicode(false);
+
+            entity.HasOne(d => d.IdDepartamentoNavigation).WithMany(p => p.Empleados)
+                .HasForeignKey(d => d.IdDepartamento)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__Empleado__id_dep__4BAC3F29");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
